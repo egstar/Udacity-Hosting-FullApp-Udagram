8717c4d6a5cbfc5ca56cb31c1260af72fcfca0f4
egstar <borham@hotmail.com>
2022-07-26 21:40:20 +0200

    initial commit

---
+++ udagram/set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=burham
+export POSTGRES_PASSWORD=b0rh4m2004
+export POSTGRES_HOST=week7task-badcats.c1wyihikfe0t.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=burham
+export AWS_BUCKET=arn:aws:s3:::burham
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=mysecretstring
+export URL=http://burham.s3-website-us-east-1.amazonaws.com:8100
