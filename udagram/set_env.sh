f1248d8628ef8ef2366d0a97c0ba37ce483078a0
egstar <borham@hotmail.com>
2022-07-27 16:40:49 +0200

    fix circleCi profile

--- udagram/set_env.sh
+++ udagram/set_env.sh
@@ -10,4 +10,4 @@ export AWS_BUCKET=arn:aws:s3:::burham
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
 export JWT_SECRET=[b0rh4m2004]3gst4r[b0rh4m43v3r]
-export URL=http://burham.s3-website-us-east-1.amazonaws.com:8100
+export URL=http://burham.s3-website-us-east-1.amazonaws.com
